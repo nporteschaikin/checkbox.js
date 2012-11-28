c211dc37bcbff24cf3c694170204f7f72e3fb312
Noah Portes Chaikin <noah@kingandpartners.com>
2012-11-28 14:45:11 -0500

    Initial commit

---
+++ checkbox.js
@@ -0,0 +1,166 @@
+/*
+
+*	checkbox.js
+* The easiest, most functional custom checkboxes around.
+* @author Noah Portes Chaikin
+* @requires jQuery 1.6+
+
+*/
+
+(function( $ ){
+	
+	var defaults = {
+		'pfx': 'checkbox'
+	},
+	
+	methods = {
+		
+		init: function ( settings ) {
+			settings = $.extend( {}, defaults, settings );
+			return this.each( 
+				function() {
+					if ( $( this ).is(':radio, :checkbox') ) {
+						
+						// input
+						var input = {
+							el: $( this ),
+							name: $( this ).attr( 'name' ),
+							tabindex: $( this ).attr('tabindex'),
+							settings: settings
+						}
+						
+						// data
+						data = $( this ).data( 'input' ) || input;
+						
+						// build
+						$build = build( data );
+						data.build = $build;
+						
+						// place
+						$( this ).hide();
+						$( this ).after( $build );
+						
+						// save data
+						$( this ).data( 'input', data );
+						$build.data( 'input', data );
+						
+						// bind elements
+						binder( data );
+						
+						// if it's selected ...
+						if ( $( this ).is(':checked') ) {
+							$build.checkbox( 'toggle' );
+						}
+						
+					}
+				}
+			)
+		},
+		
+		toggle: function () {
+			return this.each(
+				function() {
+					
+					var data = $( this ).data( 'input' );
+
+					if ( data.el ) {
+						
+						// simplify calling the input and build
+						var el = data.el,
+						build = data.build;
+						
+						if ( el.is(':checked') ) {
+							el.prop( 'checked', false );
+						} else {
+							el.prop( 'checked', true );
+						}
+						
+						// trigger change to ALL radio, checkboxes
+						// why? to set classes and values
+						$( ':radio, :checkbox' ).trigger( 'change' );
+						
+					}
+					
+				}
+			)
+		}
+		
+	}
+
+	$.fn.checkbox = function( method ) {  
+		
+		if ( methods[method] ) {
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		}
+		
+  };
+
+	function build ( data ) {
+		
+		var $build,
+		
+		// object: forced css for build
+		css = {
+			'cursor': 'pointer'
+		}
+		
+		// create element
+		$build = $('<div class="' + data.settings.pfx + '" />');
+		
+		// apply css
+		$build.css ( css );
+		
+		// set a tabindex
+		if( data.tabindex >= 0 ) {
+			$build.attr( 'tabindex', data.tabindex );
+		}
+		
+		// done! that was easy
+		return $build;
+		
+	}
+	
+	function binder ( data ) {
+		
+		var build = data.build,
+		el = data.el;
+		
+		// on click, trigger
+		build.bind( 'click', 
+			function( e ) {
+				e.preventDefault();
+				build.checkbox( 'toggle' );
+			}
+		)
+		
+		// on change, toggle
+		el.bind( 'change', 
+			function() {
+				if ( $( this ).is( ':checked' ) ) {
+					toggle ( data, true );
+				} else {
+					toggle ( data, false );
+				}
+				
+			}
+		)
+		
+	}
+	
+	function toggle ( data, bool ) {
+		
+		var open = data.settings.pfx + '-open';
+		
+		if ( bool ) {
+			data.build.addClass( open );
+		} else {
+			data.build.removeClass( open );
+		}
+		
+	}
+	
+
+
+})( jQuery );
